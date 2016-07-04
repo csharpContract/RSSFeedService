fccf80a0c997a847ef873f16a6e3824957870d4a
rahulmehta <cmehtarahul@yahoo.com>
2016-07-04 17:13:56 +0100

    angular js rss feed ui change with angular

--- RSSFeed/RSSFeed/Controllers/ValuesController.cs
+++ RSSFeed/RSSFeed/Controllers/ValuesController.cs
@@ -8,8 +8,22 @@ using System.Web.Http;
 using System.Xml;
 using static RSSFeed.RSSFeed;
 
+using System.Web.Http.Filters;
+
+public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+    {
+        if (actionExecutedContext.Response != null)
+            actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+        base.OnActionExecuted(actionExecutedContext);
+    }
+}
+
 namespace RSSFeed.Controllers
 {
+    [AllowCrossSiteJson]
     public class ValuesController : ApiController
     {
         public object FeedReader { get; private set; }
