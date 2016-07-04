ed7394e6f27421d47055143a9e60a139482624a2
rahulmehta <cmehtarahul@yahoo.com>
2016-07-04 17:15:37 +0100

    changes

---
+++ RSSFeed/RSSFeedWebApplication/Scripts/App/app.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+var App = angular.module('RSSFeedApp', []);
+
+App.controller("FeedCtrl", ['$scope', 'FeedService', function ($scope, Feed) {
+    $scope.loadButonText = "Load";
+    $scope.loadFeed = function (e) {
+        Feed.parseFeed($scope.feedSrc).then(function (res) {
+            $scope.loadButonText = angular.element(e.target).text();
+            $scope.feeds = res;
+        });
+    }
+}]);
+
+App.factory('FeedService', ['$http', function ($http) {
+        return {
+        parseFeed: function (rowcount) {
+            var promise = $http.get('http://localhost:54674/api/Values/Index/').then(function (response) {
+
+               return response.data;
+            }, function (error) {
+                alert('error');
+            })
+            return promise;
+        }
+    }
+
+}]);
