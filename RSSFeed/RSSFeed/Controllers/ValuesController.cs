797979855c89a69523378b72801f11e3fa816c59
rahulmehta <cmehtarahul@yahoo.com>
2016-07-05 20:22:30 +0100

    changes ui

--- RSSFeed/RSSFeed/Controllers/ValuesController.cs
+++ RSSFeed/RSSFeed/Controllers/ValuesController.cs
@@ -9,6 +9,7 @@ using System.Xml;
 using System.Web.Http.Filters;
 using RSSFeed.Models;
 
+
 public class AllowCrossSiteJsonAttribute : ActionFilterAttribute
 {
     public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
@@ -26,14 +27,35 @@ namespace RSSFeed.Controllers
     public class ValuesController : ApiController
     {
         public object FeedReader { get; private set; }
+        ApplicationDbContext context;
+
+        public ValuesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
 
         [HttpGet]
-        public IEnumerable<PressRSS> Index()
+        public IEnumerable<PressRSS> Index(int?id)
         {
            return  RSSFeed.RSSFeedReader.GetFeed();
         }
 
+
       
+        public IEnumerable<PressRSS> GetDropDownElements()
+        {
+            var items = context.PressRSS.Select(c => new PressRSS
+            {
+                Title = c.Title,
+                Link = c.Link
+
+            }).ToList();
+            return items;
+        }
+
+
+
         // POST api/values
         public void Post([FromBody]string value)
         {
